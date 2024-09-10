f4629c227ad9f110351412c52491bcb3bc7f2a3e
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-10 17:29:33 +0200

    feat: add file header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Layout base | @yield('titlePage')</title>
+    <title>Comics Laravel</title>
 
-    {{-- Includiamo gli assets con la direttiva @vite --}}
+    <!-- {{-- Includiamo gli assets con la direttiva @vite --}} -->
     @vite('resources/js/app.js')
 </head>
 <body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
