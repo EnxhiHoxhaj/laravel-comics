b3d510e527fa3f97fd194c4fde8e48b6b52bc5c3
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-10 21:57:27 +0200

    feat: end nav

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous'/>
     <title>Comics Laravel</title>
 
     <!-- {{-- Includiamo gli assets con la direttiva @vite --}} -->
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,42 +1,44 @@
 
     <header>
-        <div class="container">
-            <div class="logo">
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+        <div class="container hed">
+            <div class="d-flex justify-content-between align-items-center align-items-center">
+                <div class="logo col-1">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+                </div>
+                <nav class="col-6">
+                    <ul class="d-flex justify-content-between">
+                        <li>
+                            <a href="#">CHARACTER</a>
+                        </li>
+                        <li>
+                            <a href="#">COMICS</a>
+                        </li>
+                        <li>
+                            <a href="#">MOVIUE</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">CHARACTER</a>
+                        </li>
+                        <li>
+                            <a href="#">COMICS</a>
+                        </li>
+                        <li>
+                            <a href="#">MOVIUE</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">CHARACTER</a>
+                        </li>
+                        <li>
+                            <a href="#">COMICS</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">CHARACTER</a>
-                    </li>
-                    <li>
-                        <a href="#">COMICS</a>
-                    </li>
-                    <li>
-                        <a href="#">MOVIUE</a>
-                    </li>
-                    <li>
-                        <a href="#">TV</a>
-                    </li>
-                    <li>
-                        <a href="#">CHARACTER</a>
-                    </li>
-                    <li>
-                        <a href="#">COMICS</a>
-                    </li>
-                    <li>
-                        <a href="#">MOVIUE</a>
-                    </li>
-                    <li>
-                        <a href="#">TV</a>
-                    </li>
-                    <li>
-                        <a href="#">CHARACTER</a>
-                    </li>
-                    <li>
-                        <a href="#">COMICS</a>
-                    </li>
-                </ul>
-            </nav>
         </div>
     </header>
